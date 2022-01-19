0288ba4cbec8efddf644dad73d50f2bd054b4055
Berthonise Charles <berthonisecharles@Berthonises-MacBook-Air-2.local>
2022-01-19 14:34:02 -0500

    first git commit

---
+++ function.js
@@ -0,0 +1,15 @@
+function malibu(butterflies, pizza){
+    const count = butterflies + pizza
+    return count
+}
+let myBag = malibu(14, 15)
+console.log(myBag)
+let myMonth = 'March'
+
+myMonth = 'May'
+clear 
+
+
+clear//
+
+
